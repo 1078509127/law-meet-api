73d5f44c00bdd9a4274f6e64ce345fb1234a10b4
wuhong <2470395025@qq.com>
2024-07-24 19:27:16 +0800

    合并代码

--- law-meet-client/src/main/java/com/example/law/meet/client/service/SysReserveService.java
+++ law-meet-client/src/main/java/com/example/law/meet/client/service/SysReserveService.java
@@ -12,6 +12,13 @@ public interface SysReserveService {
    // 添加预约信息
    int  queryIsinserRserveInfo(WxReserveInfo wxReserveInfo) ;
 
+   // 时间冲突检测
+   Object queryReserveTmie(WxReserveInfo wxReserveInfo);
+
+   List<SysReserve> queryHistoryReserveInfo (WxReserveInfo wxReserveInfo);
+   // 添加预约信息 自己写的方法
+   Boolean add(WxReserveInfo wxReserveInfo);
+
    //预约成功通知
    List<String> approved(Integer userId);
 
